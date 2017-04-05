89a12adc7fedc39d75a19e278a84727e2b830c74
melodyVoid <botaoxyz@163.com>
2017-04-05 13:23:16 +0800

    双向数据绑定

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -6,16 +6,21 @@ angular
             this.phones = [
                 {
                     name: "Nexus S",
-                    snippet: "Fast just got faster with Nexus S."
+                    snippet: "Fast just got faster with Nexus S.",
+                    age:1
                 },
                 {
                     name: "Motorola XOOM™ with Wi-Fi",
-                    snippet: "The Next, Next Generation tablet."
+                    snippet: "The Next, Next Generation tablet.",
+                    age:2
                 },
                 {
                     name: "MOTOROLA XOOM™",
-                    snippet: "The Next, Next Generation tablet."
+                    snippet: "The Next, Next Generation tablet.",
+                    age:3
                 }
             ];
+            //设置默认值,默认选中newest
+            this.orderProp = "age";
         }
     });
